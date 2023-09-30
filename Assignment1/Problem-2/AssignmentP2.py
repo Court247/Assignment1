89759bfa048d2d0ad1a87a56ae1bfc17acebbd74
Courtney <clwoods2@miners.utep.com>
2023-09-29 22:23:30 -0600

    added comments

--- Assignment1/Problem-2/AssignmentP2.py
+++ Assignment1/Problem-2/AssignmentP2.py
@@ -5,12 +5,22 @@ from tqdm import tqdm
 import itertools
 
 def Assign1P2A(f,f3):
+    
+    # Convert the word list to a list
     wordList = list(f3)
+    
+    # Iterate through the file
     for line in f:
+        
+        # Split the line into user and hashed password
         splitLine = line.split(':')
         user = splitLine[0]
         hashedPass = splitLine[1]
+        
+        # Remove the new line character
         hashedPass = hashedPass.replace('\n',"")
+        
+        # Call the crackHash method
         crackHash(user, hashedPass, wordList)
 
 def crackHash(user, hashedPass, f2):
@@ -21,32 +31,49 @@ def crackHash(user, hashedPass, f2):
     
     print(f'Cracking password for {user}...')
     
+    # Iterate through the word list
     for word in tqdm(f2):
+        
+        # Strip the word and remove the new line character
         word = word.strip()
         word = word.replace('\n',"")
+        
+        # Call the checkWord method to check the word list
         if checkWord(word, user, hashedPass, isFound):
             isFound = True
             break
+        
+        # Call the checkNumList method to check the combination of the word and the number list
         elif checkNumList(user, word, hashedPass, numList, isFound, maxLen):
             isFound = True
             break
         
+        # Call the checkNumCombo method to check the combination of the number list
         elif checkNum == False:
             checkNum = True
             if checkNumCombo(user, numList, hashedPass, isFound):
                 isFound = True
                 break
+            
+        # Call the checkCombo method to check the combination of the word and the common password file
         if checkCombo(user, word, hashedPass,f2, isFound):
             isFound = True
             break
+        
     # if whole list is searched and the password is never found, then print that
     if(isFound == False):
         print(f'Password for {user} not in file')
 
 # Method to check the word list
 def checkWord(word, user, hashedPass, isFound):
+    
+    # Check if the word is greater than 5 characters
     if(len(word)>=5):
+        
+        # Hash the word
         hashedWord =  hex_md5.hash(word)
+        
+        # Check if the hashed word is equal to the hashed password
         if(hashedWord == hashedPass):
             isFound= True
             print(f'Password for {user} is {word}')
@@ -58,17 +85,36 @@ def checkWord(word, user, hashedPass, isFound):
 
 # Method to check the combination of the word and the number list
 def checkNumList(user, word, hashedPass, numList, isFound, maxLen):
+    
+    # Check if the length of the word is greater than 5 and less than or equal to 10
     if len(word)>5 and len(word)<=maxLen:
+        
+        # Find the difference between the length of the word and 10
         diff = maxLen-len(word)
+        
+        # Check if the difference is not equal to 0
         if diff != 0:
-            combo = list(itertools.combinations(numList, diff))
+            
+            # Find the combination of the number list
+            combo = list(itertools.permutations(numList, diff))
+            
+            # Iterate through the combination
             for i in combo:
+                
+                # Convert the tuple to string
                 x = convert(i)
+                
+                # Concatenate the word and the number list
                 temp = word+x
+                
+                # Hash the word
                 hashedWord = hex_md5.hash(temp)
+                
+                # Check if the hashed word is equal to the hashed password
                 if(hashedWord == hashedPass):
                     isFound= True
                     print(f'Password for {user} is {temp}')
+                    
                     #write to text file
                     writeText(user, temp, 'crackedPasswordsP2A.txt')
                     return isFound
@@ -76,13 +122,24 @@ def checkNumList(user, word, hashedPass, numList, isFound, maxLen):
 
 # Method to check the combination of the number list
 def checkNumCombo(user, numList, hashedPass, isFound):
+    
+    # Find the combination of the number list
     fullCombo = list(itertools.permutations(numList,8))
+    
+    # Iterate through the combination
     for num in fullCombo:
+        
+        # Convert the tuple to string
         temp = convert(num)
+        
+        # Hash the word
         hashedWord = hex_md5.hash(temp)
+        
+        # Check if the hashed word is equal to the hashed password
         if(hashedWord == hashedPass):
             isFound= True
             print(f'Password for {user} is {temp}')
+            
             #write to text file
             writeText(user, temp, 'crackedPasswordsP2A.txt') 
             
@@ -92,12 +149,21 @@ def checkNumCombo(user, numList, hashedPass, isFound):
 
 # Method to check the combination of the word and the common password file
 def checkCombo(user, word, hashedPass, f2, isFound):
+    
+    # Iterate through the common password file
     for j in f2:
+        
+        # Concatenate the word and the common password word
         temp = word+j
+        
+        # Hash the word
         hashedWord = hex_md5.hash(temp)
+        
+        # Check if the hashed word is equal to the hashed password
         if(hashedWord == hashedPass):
             isFound= True
             print(f'Password for {user} is {temp}')
+            
             #write to text file
             writeText(user, temp, 'crackedPasswordsP2A.txt')
             return isFound
--- Assignment1/Problem-2/AssignmentP2B.py
+++ Assignment1/Problem-2/AssignmentP2B.py
@@ -5,14 +5,19 @@ from tqdm import tqdm
 import itertools
 
 def Assign1P2A(f,f3):
+    # Convert the word list to a list
     wordList = list(f3)
     for line in f:
+        
+        # Split the line into user, salt and hashed password
         splitLine = line.split(':')
         user = splitLine[0]
         salt = splitLine[1]
         hashedPass = splitLine[2]
         hashedPass = hashedPass.replace('\n',"")
         hashedPass = salt + ':' + hashedPass
+        
+        # Call the crackHash method
         crackHash(user, salt, hashedPass, wordList)
 
 def crackHash(user, salt, hashedPass, f2):
@@ -23,32 +28,52 @@ def crackHash(user, salt, hashedPass, f2):
 
     print(f'Cracking password for {user}...')
 
+     # Iterate through the word list
     for word in tqdm(f2):
+        
+        # Strip the word and remove the new line character
         word = word.strip()
         word = word.replace('\n',"")
+        
+        # Call the checkWord method to check the word list
         if checkWord(word, user, salt, hashedPass, isFound):
             isFound = True
             break
+        
+        # Call the checkNumList method to check the combination of the word and the number list
         elif checkNumList(user, word, salt, hashedPass, numList, isFound, maxLen):
             isFound = True
             break
+        
+        # Call the checkNumCombo method to check the combination of the number list
         elif checkNum == False:
             checkNum = True
             if checkNumCombo(user, salt, numList, hashedPass, isFound):
                 isFound = True
                 break
+            
+        # Call the checkCombo method to check the combination of the word and the common password file
         elif checkCombo(user, word, salt, hashedPass,f2, isFound):
             isFound = True
             break
+        
     # if whole list is searched and the password is never found, then print that
     if(isFound == False):
         print(f'Password for {user} not in file')
 
 # Method to check the word list
 def checkWord(word, user, salt, hashedPass, isFound):
+    
+    # Check if the length of the word is greater than or equal to 5
     if(len(word)>=5):
+        
+        # Hash the word and salt
         hashedWord =  hex_md5.hash(salt+word)
+        
+        # Append the salt to the hashed word
         hashedWord = salt + ':' + hashedWord
+        
+        # Check if the hashed word is equal to the hashed password
         if(hashedWord == hashedPass):
             isFound= True
             print(f'Password for {user} is {word}')
@@ -60,18 +85,39 @@ def checkWord(word, user, salt, hashedPass, isFound):
 
 # Method to check the combination of the word and the number list
 def checkNumList(user, word,salt, hashedPass, numList, isFound, maxLen):
+    
+    # Check if the length of the word is greater than 5 and less than or equal to max length
     if len(word)>5 and len(word)<=maxLen:
+        
+        # Calculate the difference between the max length and the length of the word
         diff = maxLen-len(word)
+        
+        # Check if the difference is not equal to 0
         if diff != 0:
+            
+            # Calculate the combination of the number list
             combo = list(itertools.combinations(numList, diff))
+            
+            # Iterate through the combination
             for i in combo:
+                
+                # Convert the tuple to string
                 x = convert(i)
+                
+                # Append the string to the word
                 temp = word+x
+                
+                # Hash the word and salt
                 hashedWord = hex_md5.hash(salt+temp)
+                
+                # Append the salt to the hashed word
                 hashedWord = salt + ':' + hashedWord
+                
+                # Check if the hashed word is equal to the hashed password
                 if(hashedWord == hashedPass):
                     isFound= True
                     print(f'Password for {user} is {temp}')
+
                     #write to text file
                     writeText(user, temp, 'crackedPasswordsP2B.txt')
                     return isFound
@@ -79,16 +125,24 @@ def checkNumList(user, word,salt, hashedPass, numList, isFound, maxLen):
 
 # Method to check the combination of the number list
 def checkNumCombo(user, salt, numList, hashedPass, isFound):
+    # Calculate the combination of the number list
     fullCombo = list(itertools.permutations(numList,8))
+    
+    # Iterate through the combination
     for num in fullCombo:
+        
+        # Convert the tuple to string
         temp = convert(num)
+        
+        # Hash the word and salt
         hashedWord = hex_md5.hash(salt+temp)
         hashedWord = salt + ':' + hashedWord
-        if(temp == '16083058'):
-            raise Exception('Found')
+
+        # Check if the hashed word is equal to the hashed password
         if(hashedWord == hashedPass):
             isFound= True
             print(f'Password for {user} is {temp}')
+
             #write to text file
             writeText(user, temp, 'crackedPasswordsP2B.txt') 
 
@@ -117,7 +171,8 @@ def writeText(user, word,file):
 
 #Method to convert the tuple to string
 def convert(tup):
-        # initialize an empty string
+    
+    # initialize an empty string
     str = ''
     for i in tup:
         str = str + i
